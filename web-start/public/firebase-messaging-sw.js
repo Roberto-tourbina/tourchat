1d5bdac07ab723d8ac804097c7136814a4a6bc2f
Roberto Alvarez Miranda <roberto@tourbina.net>
2022-05-08 17:40:56 +0100

    delete

--- web-start/public/firebase-messaging-sw.js
+++ web-start/public/firebase-messaging-sw.js
@@ -7,26 +7,8 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/firebase-messaging-sw.js":
-/*!**************************************!*\
-  !*** ./src/firebase-messaging-sw.js ***!
-  \**************************************/
-/***/ (() => {
-
-eval("// TODO: Set up Firebase Cloud Messaging service worker//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvZmlyZWJhc2UtbWVzc2FnaW5nLXN3LmpzLmpzIiwibWFwcGluZ3MiOiJBQUFBIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vdG91ci1ieS1jaGF0Ly4vc3JjL2ZpcmViYXNlLW1lc3NhZ2luZy1zdy5qcz9lNTk5Il0sInNvdXJjZXNDb250ZW50IjpbIi8vIFRPRE86IFNldCB1cCBGaXJlYmFzZSBDbG91ZCBNZXNzYWdpbmcgc2VydmljZSB3b3JrZXIiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/firebase-messaging-sw.js\n");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
+/******/ 	"use strict";
 /******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/firebase-messaging-sw.js"]();
 /******/ 	
 /******/ })()
 ;
